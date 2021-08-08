d99df9f6f500fda226b41e84eaca8909ee13bfb6
Sarah Jean Baptiste <sjeand@hotmail.com>
2021-08-07 21:44:30 -0500

    ts examples

--- ts-examples/person.class.ts
+++ ts-examples/person.class.ts
@@ -1,5 +1,17 @@
+/*
+==========================================
+  Title: Assignment 1.4
+  Author: Professor Krasso
+  Date: 08/5/2021
+  Modified by: Sarah Jean Baptiste
+  Description: person.class.ts
+==========================================
+*/
+
+// import IPerson.
 import { IPerson } from "./person.interface";
 
+// Class to require first and last name
 class Person implements IPerson {
     firstName: string;
     lastName: string;
@@ -10,5 +22,6 @@ class Person implements IPerson {
     }
 }
 
+// Create myName variable to create new person. 
 let myName = new Person("Sarah", "Jean Baptiste");
-console.log(`My name is ${myName.firstName} ${myName.lastName}`);
+console.log(`My name is ${myName.firstName} ${myName.lastName}`); //Display "My name is" with new person variable. 
--- ts-examples/person.interface.ts
+++ ts-examples/person.interface.ts
@@ -1,3 +1,14 @@
+/*
+==========================================
+  Title: Assignment 1.4
+  Author: Professor Krasso
+  Date: 08/5/2021
+  Modified by: Sarah Jean Baptiste
+  Description: person.interface.ts
+==========================================
+*/
+
+//Export IPerson.
 export interface IPerson {
     firstName: string;
     lastName: string;
