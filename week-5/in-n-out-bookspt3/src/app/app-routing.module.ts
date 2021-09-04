2a6b9b1b4ce8e1bc5c63ad7076687ce82f35a97c
Sarah Jean Baptiste <sjeand@hotmail.com>
2021-09-04 15:51:36 -0500

    part three

---
+++ week-5/in-n-out-bookspt3/src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+/*
+==========================================
+  Title: in-n-out book
+  Author: Professor Krasso
+  Modifier: Sarah Jean Baptiste
+  Date: 09/01/2021
+  Description: part three
+==========================================
+*/
+
+//import statements
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BookListComponent
+  },
+  {
+    path: 'book-list',
+    component: BookListComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+// export class
+export class AppRoutingModule { }
